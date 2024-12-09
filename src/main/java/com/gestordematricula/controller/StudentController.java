1c74c85e33f3abc8d561174b7c97f96a2251f34a
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-09 16:05:24 -0300

    feat: cria endpoint para externalizar ações básicas

---
+++ src/main/java/com/gestordematricula/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.gestordematricula.controller;
+
+import com.gestordematricula.dto.StudentDTO;
+import com.gestordematricula.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<StudentDTO> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public StudentDTO save(@RequestBody StudentDTO studentDTO) {
+        return service.save(studentDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
