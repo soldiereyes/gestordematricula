8d5c4b56b4cf003276c24daa0b7ca2b5df94105b
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-09 16:14:54 -0300

    cria projeto spring

---
+++ src/main/java/com/gestordematricula/GestorDeMatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.gestordematricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeMatriculaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestordematricula/GestorDeMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestordematricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
