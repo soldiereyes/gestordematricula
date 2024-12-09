cf7139a593f873f022f21c543570ad881714107b
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-09 16:02:49 -0300

    feat: implementa a controller de courses com os endpoints básicos de get, post e delete

---
+++ src/main/java/com/gestordematricula/controller/CourseController.java
@@ -0,0 +1,35 @@
+package com.gestordematricula.controller;
+
+import com.gestordematricula.dto.CourseDTO;
+import com.gestordematricula.service.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+    private final CourseService service;
+
+    public CourseController(CourseService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<CourseDTO> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public CourseDTO save(@RequestBody CourseDTO courseDTO) {
+        return service.save(courseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
