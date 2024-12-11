e7d7881aae7f67a755ee401894a893178ba85728
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-11 17:08:45 -0300

    feat: implementa regra de findBy para matriculas

--- src/main/java/com/gestordematricula/repository/EnrollmentRepository.java
+++ src/main/java/com/gestordematricula/repository/EnrollmentRepository.java
@@ -1,9 +1,12 @@
 package com.gestordematricula.repository;
 
 import com.gestordematricula.model.Enrollment;
+import com.gestordematricula.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EnrollmentRepository  extends JpaRepository<Enrollment, UUID> {
+    List<Enrollment> findByStudent(Student student);
 }
